307a8265ad1a5a1232d7af0d27101cc7a63b2415
Matheus Gomes <matheusbgomes@hotmail.com>
2024-09-27 20:47:13 -0300

    Create gerarId method

--- src/Model/Aluguel/Aluguel.java
+++ src/Model/Aluguel/Aluguel.java
@@ -1,15 +1,12 @@
-package Model.aluguel;
+package Model.Aluguel;
 
 import Model.Agencia.Agencia;
 import Model.Pessoa.Cliente;
 import Model.Veiculo.GrupoVeiculo;
 import Model.Veiculo.Veiculo;
 
-import java.math.BigDecimal;
-import java.time.Instant;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.util.List;
+import java.util.UUID;
 
 public class Aluguel <T extends Veiculo<? extends GrupoVeiculo>, P extends Cliente> {
     private T veiculo;
@@ -18,14 +15,24 @@ public class Aluguel <T extends Veiculo<? extends GrupoVeiculo>, P extends Clien
     private Agencia agenciaDevolucao;
     private LocalDateTime dataAluguel;
     private LocalDateTime dataDevolucao;
+    private String id;
 
     public Aluguel(T veiculo, P pessoa, Agencia agencia, Agencia agenciaDevolucao, LocalDateTime dataAluguel, LocalDateTime dataDevolucao) {
+        this.id = gerarId();
         this.veiculo = veiculo;
         this.pessoa = pessoa;
         this.agencia = agencia;
         this.agenciaDevolucao = agenciaDevolucao;
         this.dataAluguel = dataAluguel;
         this.dataDevolucao = dataDevolucao;
+    }
+
+    private String gerarId() {
+        return UUID.randomUUID().toString().substring(0, 6);
+    }
+
+    public String getId() {
+        return id;
     }
 
     public T getVeiculo() {
